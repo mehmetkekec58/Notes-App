533ab641396ed2123bf45dcc53d8eed5648ebe69
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-04 01:37:20 +0300

    Added Styles

---
+++ Styles.js
@@ -0,0 +1,114 @@
+import { StyleSheet } from 'react-native';
+
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingTop: 30,
+
+  },
+  navigate: {
+    width: '100%',
+    backgroundColor: "#2b90c8",
+    height: "7%",
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: '0.9',
+    shadowRadius: 15.19,
+    elevation: 23,
+
+  },
+  naviBrand: {
+    fontWeight: 'bold',
+    color: 'white',
+    right: '40%',
+    top: '67%',
+    marginLeft: 40,
+    fontSize: 20,
+  },
+  login: {
+    color: 'black',
+    fontWeight: 'bold',
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 5,
+
+  },
+  button: {
+    backgroundColor: 'white',
+    padding: 5,
+    borderRadius: 5,
+    left: '44%',
+    bottom: 15,
+    fontSize: 15,
+    backgroundColor: 'rgb(0,0,0,0)'
+  },
+  image: {
+    width: 30,
+    height: 30,
+
+  },
+  input: {
+    height: 45,
+    width: "75%",
+    marginTop: 12,
+    marginLeft: 22,
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 10,
+  },
+  inputButton:
+  {
+    backgroundColor: 'white',
+    width: '12%',
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 5,
+    alignItems: 'center',
+    alignContent: 'center',
+    bottom: 42,
+    left: '82%',
+    height: 40,
+    padding: 5,
+  },
+  addButtonText: {
+    fontWeight: '600',
+    top: 9,
+  },
+  navigateStyleWithKeyboardOpen: {
+    width: '100%',
+    backgroundColor: "#2b90c8",
+    height: "10%",
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  cardText: {
+    margin: 2,
+    marginLeft: 5,
+    textAlign: 'left',
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+  scroll: {
+    bottom: 32,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 70,
+    borderRadius: 20,
+    backgroundColor: '#f0eef0',
+
+  },
+  searchImage: {
+
+  }
+});
