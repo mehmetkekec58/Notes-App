9aa635c2888c7cfcb449a1671a1a6bf6168c958a
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-04 23:57:20 +0300

    added note deletion when button press

--- components/Navi.js
+++ components/Navi.js
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import { Text, View, Image, TouchableOpacity } from 'react-native';
+import { BRAND_NAME } from '../contains/containTexts';
 import { styles } from '../Styles';
 
 const deleteIcon = "../assets/images/delete-button.png";
 const addButtonIcon = "../assets/images/addicon.png";
 
-const Navi = ({ addOrDeleteIcon, setAddOrDeleteIcon, setNoteAdd, noteAdd, style }) => {
+const Navi = ({ veri, setVeri, setSelectedId, selectedId, addOrDeleteIcon, setAddOrDeleteIcon, setNoteAdd, noteAdd, style }) => {
 
     const handleAddOrDeleteIcon = () => {
         if (addOrDeleteIcon) {
@@ -17,19 +18,46 @@ const Navi = ({ addOrDeleteIcon, setAddOrDeleteIcon, setNoteAdd, noteAdd, style 
     const addTextPage = () => {
         setNoteAdd(true)
     }
+    const handleDelete = () => {
+        setVeri(deleteData(selectedId, veri))
+        setAddOrDeleteIcon(true);
+
+    }
+    function deleteData(selectId, data) {
+        let array = [];
+        for (let i = 0; i < data.length; i++) {
+            isThere = false;
+            for (let j = 0; j < selectId.length; j++) {
+                if (data[i].id === selectId[j]) {
+                    isThere = true;
+                }
+            }
+            if (!isThere) {
+                array[array.length] = data[i]
+            }
+        }
+        return array;
+    }
+    function selectedCount() {
+        return selectedId.length;
+    }
     return (
         <View style={style}>
 
             <View>
-                <Text style={noteAdd? styles.naviBrandWithNoteAdd:styles.naviBrand}>Not Defteri</Text>
+                <Text style={noteAdd ? styles.naviBrandWithNoteAdd : styles.naviBrand}>{BRAND_NAME}</Text>
             </View>
             {noteAdd ?
                 null
                 :
                 <View>
-                    <TouchableOpacity onPress={addOrDeleteIcon ? addTextPage : null} style={styles.naviButton}>
+                
+                    <TouchableOpacity onPress={addOrDeleteIcon ? addTextPage : handleDelete} style={styles.naviButton}>
                         {addOrDeleteIcon ? <Image style={styles.image} source={require(addButtonIcon)} /> :
-                            <Image style={styles.image} source={require(deleteIcon)} />}
+                            <View>
+                              
+                                <Image style={styles.image} source={require(deleteIcon)} />
+                            </View>}
 
                     </TouchableOpacity>
                 </View>
