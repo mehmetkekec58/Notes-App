2980245f83da19a10afad339d2495e740b59f439
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-04 23:58:15 +0300

    deleted mode added for onLongPress

--- components/Card.js
+++ components/Card.js
@@ -3,17 +3,49 @@ import { Text, TouchableOpacity } from 'react-native'
 import { styles } from '../Styles';
 import selectCharacter from '../helper/selectCharacterHelper';
 
-const Card = ({ item }) => {
+const Card = ({ item, setSelectedId, selectedId, setAddOrDeleteIcon, addOrDeleteIcon }) => {
 
     const { id, message, color } = item
 
+    const handleLongPress = (id) => {
+        if (addOrDeleteIcon) {
+            setAddOrDeleteIcon(false)
+            setSelectedId([id])
+
+        } else {
+            setAddOrDeleteIcon(true)
+            setSelectedId([])
+
+        }
+    }
+    const handlePress = (id) => {
+        if (addOrDeleteIcon) {
+
+        } else {
+          
+            if (selectedId.includes(id)) {
+                setSelectedId(selectedId.filter((e) => (e !== id)));
+
+            } else {
+                setSelectedId([...selectedId, id]);
+
+            }
+
+        }
+
+    }
+    function handleSelectForDelete(selectId, id) {
+        return selectId.includes(id);
+    }
+
     return (
-        <TouchableOpacity style={{
+        <TouchableOpacity onLongPress={(e) => handleLongPress(id)} onPress={(e) => handlePress(id)} style={{
             height: 150,
             width: '45%',
             backgroundColor: color,
             borderRadius: 4,
-            borderColor: '#000',
+            borderWidth: !addOrDeleteIcon && handleSelectForDelete(selectedId, id) ? 3 : 0,
+            borderColor: 'red',
             marginLeft: 12,
             marginTop: 8,
             marginBottom: 15,
