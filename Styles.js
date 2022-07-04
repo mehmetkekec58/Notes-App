c876f41f068a4abfa0abf09b0e2ad556e9f69fae
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-04 16:20:29 +0300

    added new styles

--- Styles.js
+++ Styles.js
@@ -14,8 +14,8 @@ export const styles = StyleSheet.create({
     height: "7%",
     alignItems: 'center',
     justifyContent: 'center',
-    paddingLeft: 10,
-    paddingRight: 10,
+    paddingLeft: 12,
+    paddingRight: 20,
     shadowColor: "black",
     shadowOffset: {
       width: 0,
@@ -29,8 +29,17 @@ export const styles = StyleSheet.create({
   naviBrand: {
     fontWeight: 'bold',
     color: 'white',
-    right: '40%',
-    top: '67%',
+    right: '39%',
+    top: '63%',
+    marginLeft: 40,
+    fontSize: 20,
+
+  },
+  naviBrandWithNoteAdd: {
+    fontWeight: 'bold',
+    color: 'white',
+    right: '39%',
+    bottom:3,
     marginLeft: 40,
     fontSize: 20,
   },
@@ -42,15 +51,17 @@ export const styles = StyleSheet.create({
     padding: 5,
 
   },
-  button: {
+  naviButton: {
     backgroundColor: 'white',
     padding: 5,
     borderRadius: 5,
     left: '44%',
     bottom: 15,
     fontSize: 15,
-    backgroundColor: 'rgb(0,0,0,0)'
+    backgroundColor: 'rgb(0,0,0,0)',
+
   },
+
   image: {
     width: 30,
     height: 30,
@@ -58,7 +69,7 @@ export const styles = StyleSheet.create({
   },
   input: {
     height: 45,
-    width: "75%",
+    width: "88%",
     marginTop: 12,
     marginLeft: 22,
     borderWidth: 1,
@@ -79,9 +90,10 @@ export const styles = StyleSheet.create({
     height: 40,
     padding: 5,
   },
-  addButtonText: {
-    fontWeight: '600',
-    top: 9,
+  addButton: {
+    top: 5,
+    left: '35%',
+
   },
   navigateStyleWithKeyboardOpen: {
     width: '100%',
@@ -100,15 +112,64 @@ export const styles = StyleSheet.create({
     fontSize: 14,
   },
   scroll: {
-    bottom: 32,
+    marginTop: 10,
     paddingLeft: 10,
     paddingRight: 10,
-    paddingBottom: 70,
+    paddingBottom: 100,
     borderRadius: 20,
     backgroundColor: '#f0eef0',
 
   },
   searchImage: {
 
+  },
+  addNoteInput: {
+    height: '70%',
+    width: "90%",
+    margin: 20,
+   textAlign:'left',
+    borderWidth: 1,
+    borderRadius: 8,
+    textAlignVertical:'top',
+    padding: 10,
+   
+  },
+  saveButton: {
+    backgroundColor: '#8bd35c',
+    height: '70%',
+    width: '40%',
+    borderRadius: 8,
+    margin: 10,
+    padding:8,
+    alignItems:'center',
+    shadowColor: "black",
+    shadowOffset: {
+        width: 0,
+        height: 5,
+    },
+    shadowOpacity: '0.9',
+    shadowRadius: 15.19,
+
+    elevation: 23,
+    
+  },
+  cancelButton: {
+    backgroundColor: '#df4d60',
+    height: '70%',
+    width: '40%',
+    margin: 10,
+
+    borderRadius: 8,
+    padding:8,
+    alignItems:'center',
+    shadowColor: "black",
+    shadowOffset: {
+        width: 0,
+        height: 5,
+    },
+    shadowOpacity: '0.9',
+    shadowRadius: 15.19,
+
+    elevation: 23,
   }
 });
