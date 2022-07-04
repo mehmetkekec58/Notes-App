7273e49793d08f06b2949cbee44bbcf8fe6811b6
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-04 22:28:28 +0300

    generateId bug fixed

--- helper/generateIdHelper.js
+++ helper/generateIdHelper.js
@@ -1,4 +1,8 @@
 export default function generateId(list) {
+  
+    if(list==null ||list == undefined || list.length<=0){
+        return 0;
+    }
     let max = list[0].id;
     for (let i = 1; i < list.length; i++) {
         if (max < list[i].id) {
