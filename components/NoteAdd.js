61bfc10b094b96a0e917a6616105f236b47aaca6
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-04 16:29:09 +0300

    added Note Add component and note addi enabled

---
+++ components/NoteAdd.js
@@ -0,0 +1,68 @@
+
+import { useState, useEffect } from 'react';
+import { View, Text, Keyboard, TextInput, TouchableOpacity, Image } from 'react-native'
+import { styles } from '../Styles';
+import randomColor from '../helper/randomColorHelper';
+import generateId from '../helper/generateIdHelper';
+import { SAVE, CANCEL, WRITE_TEXT } from '../contains/containTexts';
+const crossIcon = "../assets/images/cross.png";
+const tickIcon = "../assets/images/tick.png";
+
+const NoteAdd = ({ veri, setVeri, setNoteAdd, keyboardOpen, setKeyboardOpen }) => {
+
+    const [inputText, onChangeInputText] = useState(null);
+
+    useEffect(() => {
+        const showSubscription = Keyboard.addListener("keyboardDidShow", () => {
+            setKeyboardOpen(true);
+        });
+        const hideSubscription = Keyboard.addListener("keyboardDidHide", () => {
+            setKeyboardOpen(false);
+        });
+
+        return () => {
+            showSubscription.remove();
+            hideSubscription.remove();
+        };
+    }, []);
+
+    const handleCancel = () => {
+        setNoteAdd(false)
+    }
+    const handleSave = () => {
+        let array = veri;
+        array[array.length] = { id: generateId(array), message: inputText, color: randomColor() };
+        setVeri(array)
+        handleCancel();
+    }
+
+
+    return (
+        <View style={{ display: 'flex' }}>
+
+            <TextInput
+                multiline
+                style={styles.addNoteInput}
+                onChangeText={onChangeInputText}
+                value={inputText}
+                placeholder={WRITE_TEXT}
+                onSubmitEditing={Keyboard.dismiss} />
+            <View style={{
+                flexDirection: "row",
+                flexWrap: 'wrap', left: '3%'
+            }}>
+                <TouchableOpacity onPress={handleCancel} style={styles.cancelButton}>
+                    <Image style={styles.image} source={require(crossIcon)} />
+                    <Text>{CANCEL}</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={handleSave} style={styles.saveButton}>
+                    <Image style={styles.image} source={require(tickIcon)} />
+                    <Text>{SAVE}</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+export default NoteAdd
