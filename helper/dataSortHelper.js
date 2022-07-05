5c3fa95806ff0753e944788e3d522d463ab2b15f
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-05 22:39:38 +0300

    bug fix for data Sort helper

--- helper/dataSortHelper.js
+++ helper/dataSortHelper.js
@@ -1,5 +1,7 @@
 export default function dataSort(data) {
- 
+ if (data == null || data== undefined || 0>=data.length) {
+  return data;
+ }
     data.sort(function (a, b) {
       return b.id - a.id;
     })
