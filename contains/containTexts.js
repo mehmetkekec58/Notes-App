160c8211aeb21924fe93a47b8110e667aa855656
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-05 22:47:58 +0300

    added new constains texts

--- contains/containTexts.js
+++ contains/containTexts.js
@@ -1,4 +1,8 @@
-export const BRAND_NAME="Not Defteri";
+export const BRAND_NAME = "Not Defteri";
 export const CANCEL = "İptal";
 export const SAVE = "Kaydet";
-export const WRITE_TEXT  = "Metin yaz...";
+export const WRITE_TEXT = "Metin yaz...";
+export const NO_NOTE = "Not Yok";
+export const SEARCH_NOTE = "Not Ara";
+export const SELECT_ALL = "Tümünü Seç";
+export const DROP_ALL = "Tümünü Bırak";
