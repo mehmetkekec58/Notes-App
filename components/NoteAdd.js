37e5961251e349950729417988da28e193468984
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-07 00:41:36 +0300

    added back button control and show alert to cancel

--- components/NoteAdd.js
+++ components/NoteAdd.js
@@ -1,10 +1,12 @@
 
 import { useState, useEffect } from 'react';
-import { View, Text, Keyboard, TextInput, TouchableOpacity, Image } from 'react-native'
+import { View, Text, Keyboard, TextInput, TouchableOpacity, Image, BackHandler, Alert } from 'react-native'
 import { styles } from '../Styles';
 import randomColor from '../helper/randomColorHelper';
 import generateId from '../helper/generateIdHelper';
-import { SAVE, CANCEL, WRITE_TEXT } from '../contains/containTexts';
+import { SAVE, CANCEL, WRITE_TEXT, ALERT_CANCEL, ALERT_CANCEL_BODY_TEXT, YES } from '../contains/containTexts';
+import date from '../helper/dateHelper';
+import RadioColorButton from './RadioColorButton';
 const crossIcon = "../assets/images/cross.png";
 const tickIcon = "../assets/images/tick.png";
 
@@ -28,6 +30,34 @@ const NoteAdd = ({ props }) => {
         };
     }, []);
 
+    useEffect(() => {
+
+
+        const backHandler = BackHandler.addEventListener(
+            "hardwareBackPress",
+            backAction
+        );
+
+        return () => backHandler.remove();
+    }, []);
+
+
+    const backAction = () => {
+        if (inputText == "" && noteEdit == null || noteEdit != null && noteEdit.message == inputText) {
+            handleCancel()
+            return true;
+        }
+        Alert.alert(ALERT_CANCEL, ALERT_CANCEL_BODY_TEXT, [
+            {
+                text: CANCEL,
+                onPress: () => null,
+
+            },
+            { text: YES, onPress: () => handleCancel() }
+        ]);
+        return true;
+    };
+
     const handleCancel = () => {
         setNoteAdd(false)
         if (noteEdit != null) {
@@ -39,17 +69,17 @@ const NoteAdd = ({ props }) => {
             return;
         }
         if (noteEdit != null) {
-            setVeri(veri.map((e) => e.id === noteEdit.id ? { id: noteEdit.id, message: inputText, color: noteEdit.color } : e))
+            setVeri(veri.map((e) => e.id === noteEdit.id ? { id: noteEdit.id, message: inputText, color: noteEdit.color, date: date() } : e))
             setNoteEdit(null);
             return;
         }
-        setVeri([...veri, { id: generateId(veri), message: inputText, color: randomColor() }])
+        setVeri([...veri, { id: generateId(veri), message: inputText, color: randomColor(), date: date() }])
         handleCancel();
     }
 
     return (
         <View style={{ display: 'flex' }}>
-
+            {noteEdit != null ? <Text style={styles.noteEditDateText}>{noteEdit.date}</Text> : null}
             <TextInput
                 multiline
                 style={styles.addNoteInput}
@@ -57,8 +87,9 @@ const NoteAdd = ({ props }) => {
                 value={inputText}
                 placeholder={WRITE_TEXT}
                 onSubmitEditing={Keyboard.dismiss} />
+
             <View style={styles.noteSaveButtonsView}>
-                <TouchableOpacity onPress={() => handleCancel()} style={styles.cancelButton}>
+                <TouchableOpacity onPress={() => backAction()} style={styles.cancelButton}>
                     <Image style={styles.image} source={require(crossIcon)} />
                     <Text>{CANCEL}</Text>
                 </TouchableOpacity>
