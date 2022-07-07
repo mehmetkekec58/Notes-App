a2e48f17a87549852e6961618c7bc2b4cc86078a
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-07 23:01:18 +0300

    edited color list

--- contains/colors.js
+++ contains/colors.js
@@ -1,1 +1,1 @@
-export const COLOR_LIST = ['#e95692','#00FFFF','yellow','#FF00FF','orange','#00FF00','#C0C0C0'];
+export const COLOR_LIST = ['#e95692','#00FFFF','#edff92','#FF00FF','#ff672e','#00FF00','#C0C0C0', '#ffffff'];
