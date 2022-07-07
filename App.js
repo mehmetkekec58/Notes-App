b6ea2456412447958392306b3b3e505ac4725530
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-07 23:11:10 +0300

    edited other

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, Text } from 'react-native';
 import Card from './components/Card';
 import Input from './components/Input';
 import Navi from './components/Navi';
@@ -8,97 +8,109 @@ import NoteAdd from './components/NoteAdd';
 import { styles } from './Styles';
 import dataSort from './helper/dataSortHelper';
 import NoNote from './components/NoNote';
+import { aa, retrieveData, storeData } from './services/asyncStorageService';
 
 const datas = [
   {
     id: 0,
     message: "selam dünya",
-    color: '#8eabdc',
-    date:'06/06/2022 - 23:06',
+    color: '#e95692',
+    date: '06/06/2022 - 23:06',
   },
   {
     id: 1,
     message: "React ile geliştirilecek sistem component’lerden oluşur. Her component kendi içinde kullanacağı veriyi ya props üzerinden ya da state üzerinden alarak kullanır",
     color: '#c78aad',
-    date:'06/06/2022 - 23:06',
+    date: '06/06/2022 - 23:06',
   },
   {
     id: 2,
     message: "Konu tercihlerle ilişkili bir konu, eğer react dünyasının getirdiklerini standart bir yaklaşımla kullanmak isterseniz bence değiyor",
     color: '#e1a69e',
-    date:'06/06/2022 - 23:06',
+    date: '06/06/2022 - 23:06',
   },
   {
     id: 3,
     message: "Hello World",
     color: 'grey',
-    date:'06/06/2022 - 23:06',
+    date: '06/06/2022 - 23:06',
   },
   {
     id: 4,
     message: "Redux yapısını kullanacaksanız, öncelikle Redux Dev Tool’un chrome extension’ı kurmanızı tavsiye ederim",
     color: '#f2f2f2',
-    date:'06/06/2022 - 23:06',
+    date: '06/06/2022 - 23:06',
   },
   {
     id: 5,
     message: "Redux yapısını kullanacaksanız, öncelikle Redux Dev Tool’un chrome extension’ı kurmanızı tavsiye ederim",
     color: 'brown',
-    date:'06/06/2022 - 23:06',
+    date: '06/06/2022 - 23:06',
   },
 
   {
     id: 6,
     message: "Redux yapısını kullanacaksanız, öncelikle Redux Dev Tool’un chrome extension’ı kurmanızı tavsiye ederim",
     color: 'lightblue',
-    date:'06/06/2022 - 23:06',
+    date: '06/06/2022 - 23:06',
   },
 
   {
     id: 7,
     message: "Redux yapısını kullanacaksanız, öncelikle Redux Dev Tool’un chrome extension’ı kurmanızı tavsiye ederim",
     color: '#00FF00',
-    date:'06/06/2022 - 23:06',
+    date: '06/06/2022 - 23:06',
   },
   {
     id: 8,
     message: "Hello World",
     color: 'yellow',
-    date:'06/06/2022 - 23:06',
+    date: '06/06/2022 - 23:06',
   },
   {
     id: 9,
     message: "Bilgiye ulaşmanın hem çok kolay hem de çok zor olduğu şu son zamanlarda, insanların bilgi ihtiyacını karşılamak için ille de basılı kaynaklara yönelmelerini beklemek oldukça yanlış bir düşünce biçimidir.",
     color: 'yellow',
-    date:'06/06/2022 - 23:06',
+    date: '06/06/2022 - 23:06',
   },
 ]
 
 
 
 export default function App() {
+ 
+  const [veri, setVeri] = useState([])
 
-  const [veri, setVeri] = useState(datas)
   const [addOrDeleteIcon, setAddOrDeleteIcon] = useState(true);
-  const [data, setData] = useState(veri)
+  const [data, setData] = useState(veri);
   const [noteAdd, setNoteAdd] = useState(false);
   const [keyboardOpen, setKeyboardOpen] = useState(undefined);
   const [selectedId, setSelectedId] = useState([])
   const [noteEdit, setNoteEdit] = useState(null);
 
   useEffect(() => {
+    if (veri.length == 0) {
+      retrieve();
+    }
     setData(veri);
+
   }, [veri])
-  
+
+
+  const retrieve = async () => {
+    await retrieveData("notes", setVeri)
+  }
+
+
 
   return (
     <View style={styles.container}>
-      <Navi props={{ data: data, noteEdit:noteEdit,veri: veri, setVeri: setVeri, setSelectedId: setSelectedId, selectedId: selectedId, addOrDeleteIcon: addOrDeleteIcon, setAddOrDeleteIcon: setAddOrDeleteIcon, setNoteAdd: setNoteAdd, noteAdd: noteAdd }} style={keyboardOpen ? styles.navigateStyleWithKeyboardOpen : styles.navigate} />
+      <Navi props={{ data: data, noteEdit: noteEdit, veri: veri, setVeri: setVeri, setSelectedId: setSelectedId, selectedId: selectedId, addOrDeleteIcon: addOrDeleteIcon, setAddOrDeleteIcon: setAddOrDeleteIcon, setNoteAdd: setNoteAdd, noteAdd: noteAdd }} style={keyboardOpen ? styles.navigateStyleWithKeyboardOpen : styles.navigate} />
       {noteAdd || noteEdit != null ?
         <NoteAdd props={{ noteEdit: noteEdit, setNoteEdit: setNoteEdit, veri: veri, setVeri: setVeri, setNoteAdd: setNoteAdd, keyBoardOpen: keyboardOpen, setKeyboardOpen: setKeyboardOpen }} />
         :
         (<View>
-          <Input props={{ datas: veri, data: data, setData: setData, setKeyboardOpen: setKeyboardOpen }} />
+          <Input props={{ setSelectedId: setSelectedId, selectedId: selectedId, datas: veri, data: data, setData: setData, setKeyboardOpen: setKeyboardOpen }} />
           {data == null || data == undefined || data.length <= 0 ?
             <NoNote />
             :
@@ -116,7 +128,7 @@ export default function App() {
         </View>
         )
       }
- 
+
       <StatusBar style="auto" backgroundColor='#2b90c7' />
     </View>
 
