0e2c1b4af2619f118ab644ac03ed44c13023ad19
Mehmet Kekeç <mehmet552ke@gmail.com>
2022-07-08 13:09:36 +0300

    updated async Storage

--- services/asyncStorageService.js
+++ services/asyncStorageService.js
@@ -2,11 +2,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const storeData = async (key, data) => {
     try {
-        await AsyncStorage.setItem(
-            key,
-            JSON.stringify(data)
-        );
-
+        await AsyncStorage.setItem(key,data);
+        
     } catch (error) {
 
     }
